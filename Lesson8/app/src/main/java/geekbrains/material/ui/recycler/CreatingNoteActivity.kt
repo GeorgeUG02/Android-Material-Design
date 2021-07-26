a042fb40d26e6118a12692a7af6157102a1a2dab
George <georgukladnik@gmail.com>
2021-07-26 13:02:22 +0300

    lesson8-homework

---
+++ Lesson8/app/src/main/java/geekbrains/material/ui/recycler/CreatingNoteActivity.kt
@@ -0,0 +1,25 @@
+package geekbrains.material.ui.recycler
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import geekbrains.material.R
+import kotlinx.android.synthetic.main.creating_note_activity.*
+
+class CreatingNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.creating_note_activity)
+        button3.setOnClickListener {
+             val position = intent.getIntExtra("position",0)
+             val name = nameOfNote.text
+             val note =note.text
+             setResult(RESULT_OK, Intent().apply{
+                 putExtra("position",position)
+                 putExtra("name",name)
+                 putExtra("note",note)
+             })
+             finish()
+        }
+    }
+}
